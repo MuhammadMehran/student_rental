cafd245155177c6ec101a46a6cbe39cd43529439
MuhammadMehran <mehranbutt97@gmail.com>
2022-11-01 15:09:14 +0500

    demand added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+
 st.set_page_config(layout="wide")
 
 cities = ['---------', 'Glasgow',
@@ -19,6 +20,26 @@ cities = ['---------', 'Glasgow',
 
 codes = {'---------': None, 'London': '5E87490', 'Glasgow': '5E550', 'Liverpool': '5E813', 'Manchester': '5E904',
          'Leeds': '5E787', 'Nottingham': '5E1019', 'Edinburgh': '5E475', 'Brighton': '5E61480', 'Durham': '5E460'}
+
+
+@st.cache(ttl=24*60*60)
+def get_uni_data():
+    uni_df = pd.read_csv(
+        'https://docs.google.com/spreadsheets/d/1j29ItCU8flgBlNG1U_mGNVikRTAHMJhhsloFnHpEMcU/export?gid=0&format=csv')
+    uni_df['City'] = uni_df['City'].fillna(method='ffill')
+
+    uni_df['Students / Active Listing (Avg)'] = uni_df['Students / Active Listing (Avg)'].fillna(
+        method='ffill')
+
+    uni_df['Active Listings'] = uni_df['Active Listings'].fillna(
+        method='bfill')
+
+    uni_df['Number of Students'] = uni_df['Number of Students'].str.replace(
+        ',', '').astype(float)
+    return uni_df
+
+
+uni_df = get_uni_data()
 
 
 @st.cache(ttl=24*60*60)
@@ -56,7 +77,7 @@ st.markdown(styl, unsafe_allow_html=True)
 city = st.selectbox(
     "Please select City", cities, key='city', index=0)
 df, nres = get_data(codes[city], city)
-
+uni_df_city = uni_df[uni_df['City'] == city]
 
 if df.shape[0] != 0:
     _, col1, col2, col3, col4 = st.columns((0.1, 1, 1, 1, 1))
@@ -96,6 +117,46 @@ if df.shape[0] != 0:
 
     # col2.metric(label="Average Annual Revenue", value='£ ' + str(
     #     round(df['price'].mean() * 12, 2)))
+
+
+# student.com gauge
+# if df.shape[0] != 0:
+#     if 'demand' in df.columns:
+#         demand = df['demand'].iloc[0]
+#         demand_color = df['demand_color'].iloc[0]
+#         fig = go.Figure(go.Indicator(
+#             mode="gauge+number",
+#             value=demand,
+#             domain={'x': [0, 1], 'y': [0, 1]},
+#             title={'text': "Demand"},
+#             gauge={'bar': {'color': demand_color}, 'axis': {'range': [None, 100]}}))
+#         st.plotly_chart(fig)
+
+
+if df.shape[0] != 0:
+    total_students = uni_df_city[uni_df_city['Univeristy Name']
+                                 == 'Total:'].iloc[0]['Number of Students']
+    demand = uni_df_city['Students / Active Listing (Avg)'].iloc[0]
+    _, col1, col2 = st.columns((0.04, 0.4, 1))
+    fig = go.Figure(go.Indicator(
+        mode="number", value=total_students,  number={'font_size': 60}))
+    fig.update_layout(height=200, width=300, title='Total Students')
+
+    col1.plotly_chart(fig)
+
+    fig = go.Figure(go.Indicator(
+        mode="number", value=round(demand, 2),  number={'font_size': 60}))
+    fig.update_layout(height=200, width=300,
+                      title='Students / Active Listing (Avg)')
+
+    col1.plotly_chart(fig)
+
+    uni_df_city2 = uni_df_city[uni_df_city['Univeristy Name'] != 'Total:']
+    fig = px.bar(uni_df_city2, x="Number of Students", y="Univeristy Name",
+                 color="Univeristy Name", title="Number of Students in Universities")
+    fig.update_layout(template='simple_white', width=900)
+    col2.plotly_chart(fig)
+
 
 row4_1, _, row4_spacer2 = st.columns((1, 0.1, 1))
 with row4_1:
